61040a9ab56a64046197aa4061618a81e807286b
mukherjeearnab <mukherjeearnab905@gmail.com>
2020-02-27 21:20:14 +0530

    modified home page

--- res/js/script.js
+++ res/js/script.js
@@ -114,3 +114,44 @@ $('.js--nav-icon').click(function(){
 
 
 });
+
+'use strict';
+
+$(function() {
+
+  //settings for slider
+  var width = $(".slide").width();
+  var animationSpeed = 1000;
+  var pause = 3000;
+  var currentSlide = 1;
+
+  //cache DOM elements
+  var $slider = $('#slider');
+  var $slideContainer = $('.slides', $slider);
+  var $slides = $('.slide', $slider);
+
+  var interval;
+
+  function startSlider() {
+    interval = setInterval(function() {
+      $slideContainer.animate({
+        'margin-left': '-=' + width
+      }, animationSpeed, function() {
+        if (++currentSlide === $slides.length) {
+          currentSlide = 1;
+          $slideContainer.css('margin-left', 0);
+        }
+      });
+    }, pause);
+  }
+
+  function pauseSlider() {
+    clearInterval(interval);
+  }
+
+  $slideContainer
+    .on('mouseenter', pauseSlider)
+    .on('mouseleave', startSlider);
+
+  startSlider();
+});
